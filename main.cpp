ad2df2f8ef4b1d792231017a5cf1a7fa6f105d17
genius <genius>
2018-07-28 09:27:26 +0800

    base architecture

--- main.cpp
+++ main.cpp
@@ -4,75 +4,29 @@
 #include <arpa/inet.h>
 using namespace std;
 
-int foo()
-{
-    return 1;
-}
+/*
+ * webserver1.0 -A simple. interative HTTP/1.0 Web server that uses the
+ *     Get method to serve static and dynamic content
+ */
+void doit(int fd) {}
+void read_requestdrs() {}
+void server_static(char *uri, char* filename, char *cgiargs) {}
+void get_filtype(char *filename, char* filetype){}
+void serve_dynamic(int fd, char*filename , char* cgiargs){}
+void clienterroe(int fd, char *cause, char* errnum,
+                 char*shortmsg, char* longmsg){}
+
+
+
 int main(int argc, char **argv) {
-//    int fd1,fd2;
-//    char r[10];
-//    char *p =r;
-//    int a =1 ;
-//    fd1 = open("/home/genius/Documents/Webserver1.0/foo.txt",O_RDWR,0);
-//    fd2 = open("/home/genius/Documents/Webserver1.0/foo.txt",O_RDWR,0);
-//    int n = read(fd2,&r,20);
-//    int n = rio_readn(fd1,&r,50);
-//    int n1 = rio_readn(fd1,&r,10);
-//    int a = read(fd2,&r,50);
-//    int a1 = read(fd2,&r,10);
-//    dup2(fd2,fd1);
-//    read(fd1,&r,1);
-//    cout << n<<"c" <<n1<< endl;
-//    cout << a << "c"<< a1 << endl;
-//    cout << fd1<<endl;
-//        rio_t rio;
-//    rio_readinitb(&rio,fd1);
-//    ssize_t n =rio_readlineb(&rio,r,20);
-
-//    if ((a=foo())==2)
-//        *p++ = 'c';
-//        cout << *p++ <<endl;
-//        cout << *p <<endl;
-
-//    char *src = "128.2.210.175";
-//    int ip;
-//    int ip = inet_pton(AF_INET,src,ip);
-//    exit(0);
-//    struct in_addr inaddr;
-//    uint32_t addr;
-//    char buf[MAXBUF];
-//     if(argc!= 2)
-//         exit(0);
-//     sscanf(argv[1],"%x", &addr);
-//     inaddr.s_addr = htonl(addr);
-//     inet_ntop(AF_INET,&inaddr,buf,MAXBUF);
-//     cout << buf<< endl;
-//     sockaddr_in sock;
-//     unsigned char sin[8];
-//     cout << sizeof(inaddr) <<endl;
-    struct addrinfo *p, *listp ,hints;
-    char buf[MAXBUF];
-    int rc, flags;
-//    if (argc !=2)
-//    {
-//        cout << stderr << argv[0];
-//    }
-    argv[1] = "wwww.baidu.com";
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family  = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    if ((rc = getaddrinfo(argv[1], NULL, &hints, &listp))!=0)
-    {
-        fprintf(stderr, "geraddrinfo error :%s\n" ,gai_strerror(rc));
-        exit(1);
-    }
-    flags = NI_NUMERICHOST;
-    for(p = listp;p; p = p->ai_next) {
-        getnameinfo(p->ai_addr, p->ai_addrlen, buf, MAXBUF, NULL, 0, flags);
-        cout<< buf <<endl;
-    }
-    freeaddrinfo(listp);
-    exit(0);
+    void doit(int fd);
+    void read_requestdrs(rio_t *rp);
+    void parse_uir(char*uri, char*filename, char*cgiargs);
+    void serve_static(int fd, char* filename, int filesize);
+    void get_filtype(char *filename, char* filetype);
+    void serve_dynamic(int fd, char*filename , char* cgiargs);
+    void clienterroe(int fd, char *cause, char* errnum,
+                        char*shortmsg, char* longmsg);
 
 
 
--- opensocket.cpp
+++ opensocket.cpp
@@ -41,7 +41,7 @@ int open_listenfd(char* hostname, char* port) {
     for( p =listp;p;p = p->ai_next){
         if((listenfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))<0)
             continue;
-        Setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,(const void*)&optval, sizeof(int));
+        setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,(const void*)&optval, sizeof(int));
         if(bind(listenfd,p->ai_addr,p->ai_addrlen)==0)
             break;
         close(listenfd);
